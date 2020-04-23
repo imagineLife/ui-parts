d54d1ec4659c1bd2b43144496a5e7ba2702392df
Eric Laursen <ericjlaursen@gmail.com>
2020-04-23 06:03:51 -0400

    passes routes to Home route

--- src/App/App.js
+++ src/App/App.js
@@ -8,20 +8,51 @@ import {
 import Sidebars from './../Routes/Sidebar';
 import Widgets from './../Routes/Widgets';
 import Home from './../Routes/Home'
+import DataLoader from './../Routes/DataLoader'
+
+//single-source of routes && Components to Lookup
+const RoutesLookup = [
+  {
+    component: Sidebars,
+    path: '/sidebars'
+  },
+  {
+    component: Widgets,
+    path: '/widgets'
+  },
+  {
+    component: DataLoader,
+    path: '/csvLoader'
+  },
+  {
+    component: Home,
+    path: '/',
+  },
+
+]
 
 export default function App() {
   return (
     <Router>
         <Switch>
-          <Route path="/sidebars">
-            <Sidebars />
-          </Route>
-          <Route path="/widgets">
-            <Widgets />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
+          {
+            RoutesLookup.map((r,idx) => {
+              const ThisRoute = r.component
+              let props = r.props ? r.props : {}
+              if(r.path === '/'){
+                props = {
+                  routes: [...RoutesLookup]
+                }
+              }
+              return (
+                <Route 
+                  key={`${r.component}-${idx}`}
+                  path={r.path}>
+                  <ThisRoute {...props} />
+                </Route>
+              )
+            })
+          }
         </Switch>
     </Router>
   );
