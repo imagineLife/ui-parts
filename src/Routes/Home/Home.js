cbd7deb11329a8c30ec5626edd8117e0c4c90b65
Eric Laursen <ericjlaursen@gmail.com>
2020-04-23 06:03:31 -0400

    render li links from props map

--- src/Routes/Home/Home.js
+++ src/Routes/Home/Home.js
@@ -1,25 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-const Home = () => (
-	<nav>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/about">About</Link>
-      </li>
-      <li>
-        <Link to="/users">Users</Link>
-      </li>
-      <li>
-        <Link to="/sidebars">Sidebars</Link>
-      </li>
-      <li>
-        <Link to="/widgets">Widgets</Link>
-      </li>
-    </ul>
-  </nav>
-);
+const Home = ({routes}) => {  
+  return (
+    <nav>
+      <ul>
+        {routes.map((r, idx) => (
+          <li key={`${r.path}-${idx}`}>
+            <Link to={r.path}>{r.component.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  )
+};
 
 export default Home;
