70f13c919fb6aa1b1374e0f910ff9c4983812b16
Eric Laursen <ericjlaursen@gmail.com>
2020-04-24 19:38:24 -0400

    renders table using table component

--- src/Components/DataTable/DataTable.js
+++ src/Components/DataTable/DataTable.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+
+import { useTable } from 'react-table'
 
 const DataTable = ({tableData}) => {
-	console.log('tableData')
-	console.log(tableData)
-	
-	return(<p>DataTable</p>)
+	const colDefs = useMemo(() => tableData.headers)
+	const rowDefs = useMemo(() => tableData.rows)
+	const {
+  getTableProps,
+  getTableBodyProps,
+  headerGroups,
+  rows,
+  prepareRow,
+} = useTable({ columns: colDefs, data: rowDefs })
+				
+	// Render the UI for your table
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map(headerGroup => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row, i) => {
+          prepareRow(row)
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map(cell => {
+                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+              })}
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
 };
 
 export default DataTable;
