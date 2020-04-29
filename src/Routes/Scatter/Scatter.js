0a8e3c914006601e1e9fba646b9a37e491d8e20a
Eric Laursen <ericjlaursen@gmail.com>
2020-04-29 18:29:17 -0400

    builds x && y stats

--- src/Routes/Scatter/Scatter.js
+++ src/Routes/Scatter/Scatter.js
@@ -1,11 +1,44 @@
 import React from 'react';
+import Chart from './../../Components/Chart';
 
 const Scatter = ({x, y}) => {
-	console.log('x')
-	console.log(x)
-	console.log('y')
-	console.log(y)
+
+	//prep min/max && paired incoming data
+	let xStats = {
+		min: null,
+		max: null
+	}
+	let yStats = {
+		min: null, 
+		max: null,
+	}
+
+	let resData = []
+
+	//loop through incoming data && update prepped objs above
+	x.forEach((xElm, xIdx) => {
+		if(xElm < xStats.min || xStats.min === null) xStats.min = xElm;
+		if(xElm > xStats.max || xStats.max === null) xStats.max = xElm;
+		if(y[xIdx] < yStats.min || yStats.min === null) yStats.min = y[xIdx];
+		if(y[xIdx] > yStats.max || yStats.max === null) yStats.max = y[xIdx];
+		resData.push({
+			x: xElm,
+			y: y[xIdx]
+		})
+	})
+
+	console.log('resData')
+	console.log(resData)
+	console.log('xStats')
+	console.log(xStats)
+	console.log('yStats')
+	console.log(yStats)
 	
+	//merge both sets with keys x && y
+	// get linear scales of both x && y
+	// get min && max of both x & y
+	
+
 	return(<p>Scatter</p>)
 };
 
