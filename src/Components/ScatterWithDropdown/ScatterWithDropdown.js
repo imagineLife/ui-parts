3d726c36051f1c9246e1f98bf4c4acabdead6100
Eric Laursen <ericjlaursen@gmail.com>
2020-05-01 05:52:16 -0400

    y-axis btn + DD

--- src/Components/ScatterWithDropdown/ScatterWithDropdown.js
+++ src/Components/ScatterWithDropdown/ScatterWithDropdown.js
@@ -5,20 +5,21 @@ const ScatterWithDropdown = ({data}) => {
 	const [xAxisColumn, setXaxisColumn] = useState(null)
 	const [yAxisColumn, setYaxisColumn] = useState(null)
 	const [showXDD, setShowXDD] = useState(false)
+	const [showYDD, setShowYDD] = useState(false)
 
 	console.log('data')
 	console.log(data)
 	
+	let xAxisPicker;
 	if(!xAxisColumn){
-		return(
-			<Fragment>
+		xAxisPicker = (
 				<div className="dropdown">
 				  <button 
 				  	onClick={() => {
 				  		setShowXDD(!showXDD)
 				  	}} 
 				  	className="dropbtn">
-				  		Dropdown
+				  		X-Axis Value
 				  	</button>
 				  <ul id="myDropdown" className={`dropdown-content ${showXDD ? 'open': ''}`}>
 				    {data.headers.map((h, hIdx) => (
@@ -34,10 +35,56 @@ const ScatterWithDropdown = ({data}) => {
 				    ))}
 				  </ul>
 				</div>
-			</Fragment>
 		)
 	}
-	return(<p>Scatter With Dropdown</p>)
+
+	let yAxisPicker;
+	if(!yAxisColumn){
+		yAxisPicker = (
+			<div className="dropdown">
+			  <button 
+			  	onClick={() => {
+			  		setShowYDD(!showYDD)
+			  	}} 
+			  	className="dropbtn">
+			  		Y-Axis Value
+			  	</button>
+			  <ul id="myDropdown" className={`dropdown-content ${showYDD ? 'open': ''}`}>
+			    {data.headers.map((h, hIdx) => (
+			    	<li
+			    		key={`x-data-${h.Header}`}
+			    		onClick={() => {
+			    			setYaxisColumn(h.Header)
+			    			setShowYDD(false)
+			    		}}
+			    	>
+			    		{h.Header}
+			    	</li>
+			    ))}
+			  </ul>
+			</div>
+		)
+	}
+
+	if(xAxisColumn){
+		xAxisPicker = <Fragment>
+			<span className="chosen-axis">X-Axis</span>
+			<span className="chosen-axis-value">{xAxisColumn}</span>
+		</Fragment>
+	}
+
+	if(yAxisColumn){
+		yAxisPicker = <Fragment>
+			<span className="chosen-axis">Y-Axis</span>
+			<span className="chosen-axis-value">{yAxisColumn}</span>
+		</Fragment>
+	}
+	return(
+		<Fragment>
+			<p>Scatter With Dropdown</p>
+			{xAxisPicker}
+			{yAxisPicker}
+		</Fragment>)
 };
 
 export default ScatterWithDropdown;
