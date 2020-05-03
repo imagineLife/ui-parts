b62c4b01b1498ac2a1f691ea19d1578bd5c310ec
Eric Laursen <ericjlaursen@gmail.com>
2020-05-03 13:33:56 -0400

    removes unused code, adds debug cmd

--- process/1/reviewData.js
+++ process/1/reviewData.js
@@ -1,7 +1,10 @@
 const fs = require('fs')
 const rl = require('readline')
+const util = require('util')
+const debug = util.debuglog('review')
 
 const jsonParseFile = (fileStr) => {
+	debug('\x1b[32m%s\x1b[0m',`jsonParseFile`)
 	return new Promise((resolve,reject) => {
 		var lineReader = rl.createInterface({
 		  input: fs.createReadStream(fileStr)
@@ -10,86 +13,38 @@ const jsonParseFile = (fileStr) => {
 		let resData = []
 		lineReader.on('line', function (line) {
 				let csvArr = line.split(',');
-				resData.push(csvArr)
+				resData = csvArr
 		});
 
 		lineReader.on('close', () => {
-			console.log('CLOSED');	
+			debug('\x1b[32m%s\x1b[0m',`DONE parsing input`)
 			resolve(resData)
 		});
 	})
 }
 
-const lessMatchingCols = (data, str, caseSensitive) => {
-	let resArr = []
-}
+const groupIntoCategories = (srcArr) => {
+	debug('\x1b[32m%s\x1b[0m',`groupIntoCategories`)
+	//result placeholder
+	let groupedObj = {
+		age: [],
+		sex: [],
+		income: [],
+		education: [],
+		race: []
+	}
 
-const filterColumnsByString = (nestedArr, str) => {
-	
-	//store array of arrays
-	let resArr = []
-	
-	let columnIndexesToSkip = []
-	nestedArr.forEach((dataRow, idx) => {
-		//store filtered-row data
-		let thisRowArr = []
-		
-		//looping through header
-		if(idx === 0){
-			dataRow.forEach((rowCell,cellIdx) => {
-				
-				//check if header-cell matches passed string
-				let thisCellIsOk = !rowCell.includes(str)
-
-				if(thisCellIsOk){
-					thisRowArr.push(rowCell)
-				}else{
-					columnIndexesToSkip.push(cellIdx)
-				}
-			})
-
-		// looping through data rows, NOT header
-		}else{
-			dataRow.forEach((rowCell,cellIdx) => {
-				//assure this cell index is not cell to be skipped
-				if(!columnIndexesToSkip.includes(cellIdx)){
-					thisRowArr.push(rowCell)
-				}
-			})
+	srcArr.map((header, headerIdx) => {
+		//AGE
+		if(header.match(/AGE:/)){
+			groupedObj.age.push({title: header, idx: headerIdx})
 		}
-
-		//store row array in parent array
-		resArr.push(thisRowArr)
 	})
-	
-	return resArr
-}
-
-const makeIntoCSV = (nestedArr) => {
-	let resStr = ''
-	let columns = 0
-	let rows = 0
-	nestedArr.forEach((row, rowIdx) => {
-		let thisRowStr = ''
-		row.forEach((rowCell, cellIdx) => {
-			if(rowIdx === 0){
-				columns = columns + 1;
-			}
-
-			let rowAndThisCell = thisRowStr + rowCell
-			if(cellIdx !== row.length - 1){
-				thisRowStr =  rowAndThisCell.trim() + ','
-			}else{
-				thisRowStr = rowAndThisCell.trim() + '\n'
-			}
-		})
-		rows = rows + 1;
-		resStr = resStr += thisRowStr
-	})
-	return resStr
+	return groupedObj
 }
 
 jsonParseFile('./../../src/mockData/justHeaderRow.csv').then(headerData => {
-	console.log('headerData')
-	console.log(headerData)
+	debug('\x1b[32m%s\x1b[0m',`jsonParseFile THEN`)
+	let sorted = headerData.sort()
+	let grouped = groupIntoCategories(sorted)
 })
