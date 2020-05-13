dcb53c5b2b184d777e441e1348278103564cc55a
Eric Laursen <ericjlaursen@gmail.com>
2020-05-13 05:42:34 -0400

    renders canvas

--- src/Components/ColorLegend/ColorLegend.js
+++ src/Components/ColorLegend/ColorLegend.js
@@ -1,31 +1,27 @@
 import React, { useEffect, useState} from 'react';
 import * as ds from 'd3-scale';
+import * as dsc from 'd3-scale-chromatic'
 
 const ColorLegendComp = (props) => {
-	const [colorScale, setColorScale] = useState(null)
 
-	// build colorScale?!
-	useEffect(() => {
-		if(!colorScale){
-			// let thisScale = ds.scaleSequential(ds.interpolateGreens);
-			// console.log('thisScale')
-			// console.log(thisScale)
-			console.log('ds.interpolateGreens')
-			console.log(ds.interpolateGreens)
-			
-			setColorScale(true)
-		}
-	})
+	//prepare the d3 color-scale
+	const colorScale = ds.scaleSequential(dsc.interpolateGreens)
 	
 	const h = props.size && props.size.h || 150;
 	const w = props.size && props.size.w || 500;
 
-	return (<svg 
-		className="color-legend"
-		style={{
-			height: h,
-			width: w
-		}}>color legend comp</svg>)
+	return (<div>
+		<canvas 
+			style={{
+				height: h,
+				width: w
+			}}
+			className="color-canvas" />
+		<svg 
+		className="color-legend">
+			color legend comp
+		</svg>
+	</div>)
 };
 
 export default ColorLegendComp;
