66d4ffc5c64258d11d21f68508d76eab27922d97
Eric Laursen <ericjlaursen@gmail.com>
2020-05-13 06:02:45 -0400

    beginning creating color in canvas

--- src/Components/ColorLegend/ColorLegend.js
+++ src/Components/ColorLegend/ColorLegend.js
@@ -5,6 +5,8 @@ import React, {
 } from 'react';
 import * as ds from 'd3-scale';
 import * as dsc from 'd3-scale-chromatic'
+import * as d3Color from 'd3-color';
+import * as d3Arr from 'd3-array'
 
 const ColorLegendComp = (props) => {
 
@@ -18,7 +20,7 @@ const ColorLegendComp = (props) => {
 
 			const canvasObj = colorScaleRef.current
 			//prepare the d3 color-scale
-			const colorScale = ds.scaleSequential(dsc.interpolateGreens)
+			const colorScale = ds.scaleSequential(dsc.interpolateGreens).domain([0,100])
 			
 			// https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/getContext
 			var canvasContext = canvasObj.getContext("2d");
@@ -26,6 +28,19 @@ const ColorLegendComp = (props) => {
 			//... based on http://bl.ocks.org/mbostock/048d21cf747371b11884f75ad896e5a5
 		  var image = canvasContext.createImageData(1, props.size.h || 150);
 
+		  d3Arr.range(props.size.w || 500).forEach(function(i) {
+          const c = d3Color.rgb(
+          	colorScale(
+          		ds.scaleLinear().domain([0,100]).invert(i)
+          	)
+          );
+          image.data[4*i] = c.r;
+          image.data[4*i + 1] = c.g;
+          image.data[4*i + 2] = c.b;
+          image.data[4*i + 3] = 255;
+        });
+
+        canvasContext.putImageData(image, 0, 0);
 		  setColorApplied(true)
 
 		}
