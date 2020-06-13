28af653be006583f08eab9f5fca6246bd344d04c
Eric Laursen <ericjlaursen@gmail.com>
2020-06-13 11:40:53 -0400

    new var name

--- src/Routes/Map/Map.js
+++ src/Routes/Map/Map.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import MapComp from './../../Components/Map'
-const Map = () => (
+const MapRoute = () => (
 	<div id="map-route">
 		<p>Map Route</p>
 		<MapComp />
 	</div>);
 
-export default Map;
+export default MapRoute;
