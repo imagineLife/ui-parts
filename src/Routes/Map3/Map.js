02e7c38b97a8ccf3c4890f5a479b9b1c7fd69591
Eric Laursen <ericjlaursen@gmail.com>
2020-06-14 18:32:11 -0400

    third-map-route

---
+++ src/Routes/Map3/Map.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import MapComp from './../../Components/MapThird'
+const MapRoute = () => (
+	<div id="map-route">
+		<p>MapTwo Route</p>
+		<MapComp />
+	</div>);
+
+export default MapRoute;
