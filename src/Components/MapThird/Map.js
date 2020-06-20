56d8e9f27385404ea715095a4487a6bbd22c4837
Eric Laursen <ericjlaursen@gmail.com>
2020-06-20 19:45:42 -0400

    renders geojson using d3 + react

--- src/Components/MapThird/Map.js
+++ src/Components/MapThird/Map.js
@@ -3,17 +3,19 @@ import * as dg from 'd3-geo'
 import * as d3Select from 'd3-selection'
 import * as d3Z from 'd3-zoom'
 import './Map.css'
-
+import Rtt from 'react-tooltip'
 // https://github.com/topojson/topojson-client
 import * as topo from "topojson-client"
 
 // https://github.com/topojson/us-atlas
-import topofile from './../../mockData/states-10m.json'
+import topofile from './../../mockData/states-geojson.json'
 
-const Map = () => {
+const MapComponent = () => {
 	
 	const [divSize] = useState({w: 975, h: 610})
 	const [paths, setPaths] = useState(null)
+	const [tooltipText, setTooltipText] = useState(null)
+	const [tooltipClass, setTooltipClass] = useState('tooltip hidden');
 	const svgRef = useRef()
 	const gRef = useRef()
 
@@ -29,79 +31,86 @@ const Map = () => {
 			gRef.current
 		){
 
+			const pathFillFn = d => {
+
+		      // Get data value
+		      const value = d.properties.visited;
+
+		      if (value) {
+		      //If value exists…
+		      return legendColorScale(value);
+		      } else {
+		      //If value is undefined…
+		      return "rgb(213,222,217)";
+		      }
+		  }
+
+			const enterStates = e => {
+		    e.append("path")
+		    .attr("d", d3Path)
+		    .style("stroke", "#fff")
+		    .style("stroke-width", "1")
+		    .style('vector-effect', 'non-scaling-stroke')
+		    .attr('class', 'boundary')
+		    .on('mouse')
+		  }
+
+		  const updateStates = u => {
+		  	//zoom fn
+				const zoomed = () => {
+		        d3Select.selectAll('path')
+		        .attr('transform', d3Select.event.transform);
+		    }
+
+				const zoom = d3Z.zoom()
+		      .scaleExtent([1, 8])
+		      .on('zoom', zoomed);
+
+		    d3Select.select('#map-box').call(zoom);
+		  }
+
+
 			//get svg && g elements in d3-land
-			const d3SVG = d3Select.select(svgRef.current)
-			const d3g = d3Select.select(gRef.current)
+			const d3SVG = d3Select.select('#map-box')
+			const d3g = d3Select.select('#g-wrapper')
 
-			var tooltip = d3Select.select("#map")
-         .append("div")
-         .attr("class", "tooltip hidden");
-
-			console.log('topofile.objects.states')
-			console.log(topofile.objects.states)
-			
-			function showTooltip(d) {
-				console.log('SHOWING...');
-	      let label = d.properties.name;
-	      var mouse = d3Select.mouse(d3SVG.node())
-	        .map(d => parseInt(d));
-	      tooltip.classed("hidden", false)
-	        .attr("style", "left:"+(mouse[0]+offsetL)+"px;top:"+(mouse[1]+offsetT)+"px")
-	        .html(label);
-	    }
-			const d3Path = dg
-			.geoPath()
-			.projection(
-				dg
-					.geoAlbersUsa()
-					.translate([487.5, 305]).scale([800]))
-			
-			let states = d3g.selectAll("path")
-          .data(topofile.objects.states.geometries)
-          .enter().append("path")
-          // .attr("name", function(d) { return d.properties.name;})
-          .attr('stroke', strokeColor)
-					.attr('stroke-linejoin', "round")
-					.attr('stroke-linecap', "round")
-					.attr('stroke-width', strokeW)
-          .attr("d", d => d3Path(topo.mesh(topofile, d)))
-          // .attr("id", function(d) { return d.id;})
-          // .on('click', selected)
-          .on("mousemove", showTooltip)
-          // .on("mouseout",  function(d,i) {
-          //     tooltip.classed("hidden", true);
-          //  });
-
-			//zoom fn
-			const zoomed = () => {
-	      d3g
-	        .selectAll('path') // To prevent stroke width from scaling
-	        .attr('transform', d3Select.event.transform);
-	    }
-
-			const zoom = d3Z.zoom()
-	      .scaleExtent([1, 8])
-	      .on('zoom', zoomed);
-
-	    d3SVG.call(zoom);
+	   	const statePathsDJ = d3SVG
+	    	.selectAll("path")
+	    	.data(topofile.features)
+  		statePathsDJ.join(enterStates, updateStates)
 		}
 	})
+
+	const myProjection = dg
+		.geoAlbersUsa()
+		.translate([487.5, 305]).scale([800]);
+
+	const d3Path = dg
+		.geoPath()
+		.projection(myProjection);
 	
 	return(
 		<div id="map-wrapper">
 			<svg 
 				viewBox="0 0 975 610"
 				id="map-box"
-				ref={svgRef}>
-					<g 
-						fill="none" 
-						stroke="#000" 
-						strokeLinejoin="round" 
-						strokeLinecap="round"
-						ref={gRef}
-					/>
+				ref={svgRef}
+			>
+				<g 
+					fill="none" 
+					id="g-wrapper"
+					stroke="#000" 
+					strokeLinejoin="round" 
+					strokeLinecap="round"
+					ref={gRef}
+				/>
 			</svg>
+			<div 
+				id="tooltip" 
+				className={tooltipClass}>
+					{tooltipText}
+			</div>
 		</div>)
 };
 
-export default Map;
+export default MapComponent;
