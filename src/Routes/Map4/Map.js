670c88ac464cd01e8a891673ce89dde4a50e4a93
Eric Laursen <ericjlaursen@gmail.com>
2020-06-21 20:39:23 -0400

    init

---
+++ src/Routes/Map4/Map.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import MapComp from './../../Components/MapFourth'
+const MapRoute = () => (
+	<div id="map-route">
+		<p>MapFour Route, D3 doing heavy-lifting on TopoJSON data</p>
+		<MapComp />
+	</div>);
+
+export default MapRoute;
