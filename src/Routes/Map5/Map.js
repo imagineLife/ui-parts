2f957bbff6eed7599be9241bf06f5896a887f34c
Eric Laursen <ericjlaursen@gmail.com>
2020-06-21 20:39:35 -0400

    zoom onClick

---
+++ src/Routes/Map5/Map.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import MapComp from './../../Components/MapFifth'
+const MapRoute = () => (
+	<div id="map-route">
+		<p>MapFive, D3 TopoJson, Click-to-zoom</p>
+		<MapComp />
+	</div>);
+
+export default MapRoute;
