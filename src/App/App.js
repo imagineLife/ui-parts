e201e918885adbfd09eba333ebec16651bb7793a
Eric Laursen <ericjlaursen@gmail.com>
2020-06-22 06:24:42 -0400

    limited routes

--- src/App/App.js
+++ src/App/App.js
@@ -6,58 +6,73 @@ import {
 } from "react-router-dom";
 
 // Components
-import Sidebars from './../Routes/Sidebar';
-import Widgets from './../Routes/Widgets';
-import Home from './../Routes/Home'
-import DataLoader from './../Routes/DataLoader'
-import CsvLoader from './../Routes/CsvLoader'
-import Scatter from './../Routes/Scatter';
+// import Sidebars from './../Routes/Sidebar';
+// import Widgets from './../Routes/Widgets';
+// import DataLoader from './../Routes/DataLoader'
+// import CsvLoader from './../Routes/CsvLoader'
+// import Scatter from './../Routes/Scatter';
+// import Mapper from './../Routes/Map';
+// import MapperTwo from './../Routes/Map2';
+// import MapperThree from './../Routes/Map3';
+// import MapperFour from './../Routes/Map4';
+import MapperFive from './../Routes/Map5';
+import MapperSix from './../Routes/Map6';
 import ColorLegend from './../Routes/ColorLegend';
-import Mapper from './../Routes/Map';
-import MapperTwo from './../Routes/Map2';
-import MapperThree from './../Routes/Map3';
+import Home from './../Routes/Home'
 
 // data
 import percBelow from './../mockData/percBelowPov.json'
 import percBelowMale from './../mockData/percBelowPovMale.json'
 //single-source of routes && Components to Lookup
 const RoutesLookup = [
+  // {
+  //   component: Sidebars,
+  //   path: '/sidebars'
+  // },
+  // {
+  //   component: Widgets,
+  //   path: '/widgets'
+  // },
+  // {
+  //   component: DataLoader,
+  //   path: '/tsvLoader'
+  // },
+  // {
+  //   component: CsvLoader,
+  //   path: '/csvLoader'
+  // },
+  // {
+  //   component: Scatter,
+  //   path: '/scatter',
+  //   props: {
+  //     x: percBelow,
+  //     y: percBelowMale
+  //   }
+  // },
+  // {
+  //   component: Mapper,
+  //   path:'/map'
+  // },
+  // {
+  //   component: MapperTwo,
+  //   path:'/map2'
+  // },
+  // {
+  //   component: MapperThree,
+  //   path:'/map3'
+  // },
+  // {
+  //   component: MapperFour,
+  //   path:'/map4'
+  // },
   {
-    component: Sidebars,
-    path: '/sidebars'
+    component: MapperFive,
+    path:'/map5'
   },
   {
-    component: Widgets,
-    path: '/widgets'
-  },
-  {
-    component: DataLoader,
-    path: '/tsvLoader'
-  },
-  {
-    component: CsvLoader,
-    path: '/csvLoader'
-  },
-  {
-    component: Scatter,
-    path: '/scatter',
-    props: {
-      x: percBelow,
-      y: percBelowMale
-    }
-  },
-  {
-    component: Mapper,
-    path:'/map'
-  },
-  {
-    component: MapperTwo,
-    path:'/map2'
-  },
-  {
-    component: MapperThree,
-    path:'/map3'
-  },
+    component: MapperSix,
+    path:'/map6'
+  },  
   {
     component: ColorLegend,
     path: '/colorLegend',
