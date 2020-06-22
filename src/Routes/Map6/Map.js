4f8c3d1278b7e6be0272146f5f5f5b0120454a36
Eric Laursen <ericjlaursen@gmail.com>
2020-06-22 06:24:50 -0400

    init

---
+++ src/Routes/Map6/Map.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import MapComp from './../../Components/MapSixth'
+const MapRoute = () => (
+	<div id="map-route">
+		<p>MapSix, D3 TopoJson, Click-to-zoom, show/hide state boundaries</p>
+		<MapComp />
+	</div>);
+
+export default MapRoute;
