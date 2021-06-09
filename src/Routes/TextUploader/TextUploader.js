6bca890322c1cc8b63a43e1bd380f7e93b93241e
Eric Laursen <ericjlaursen@gmail.com>
2021-06-08 21:06:32 -0400

    link to more text

--- src/Routes/TextUploader/TextUploader.js
+++ src/Routes/TextUploader/TextUploader.js
@@ -1,5 +1,12 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
-const TextUploader = () => (<p>TextUploader</p>);
+const TextUploader = () => {
+  return(
+    <Fragment>
+      <p>See <a target="_blank" href="https://avalon.law.yale.edu/subject_menus/inaug.asp">This Link</a> for more historical presidential inaugural addresses.</p>
+      <input type="file" id="file-selector" multiple/>
+    </Fragment>
+  )
+};
 
 export default TextUploader;
