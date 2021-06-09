2bb814269785a18ae9206ac38c278a5a4579818f
Eric Laursen <ericjlaursen@gmail.com>
2021-06-08 21:13:07 -0400

    more

--- src/Routes/TextUploader/TextUploader.js
+++ src/Routes/TextUploader/TextUploader.js
@@ -1,10 +1,27 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useRef, useEffect } from 'react';
 
 const TextUploader = () => {
+  const inputRef = useRef()
+
+  const inputChange = (e) => {
+    const fileList = event.target.files;
+    console.log(fileList);
+  }
+
+  useEffect(() => {
+    inputRef.current.addEventListener('change', inputChange);
+    console.log('Event Listener registered')
+  },[])
+
   return(
     <Fragment>
       <p>See <a target="_blank" href="https://avalon.law.yale.edu/subject_menus/inaug.asp">This Link</a> for more historical presidential inaugural addresses.</p>
-      <input type="file" id="file-selector" multiple/>
+      <input 
+        ref={inputRef} 
+        type="file" 
+        id="file-selector" 
+        multiple
+      />
     </Fragment>
   )
 };
