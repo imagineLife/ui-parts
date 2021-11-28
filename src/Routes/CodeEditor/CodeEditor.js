32858a7d40605177322cdcf0c67d70ce5aba8942
Eric Laursen <ericjlaursen@gmail.com>
2021-11-28 09:08:16 -0500

    init cols

--- src/Routes/CodeEditor/CodeEditor.js
+++ src/Routes/CodeEditor/CodeEditor.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import './CodeEditor.css';
 
-const CodeEditor = () => (<p>CodeEditor</p>);
+const CodeEditor = () => (
+  <main id="code-editor">
+
+    <h2>Inline Code Editor + HTML Output</h2>
+    <p>On the left, a div rendering some content.</p>
+    <p>On the right, an input for you the viewer to edit & update the output</p>
+  
+    <section id="columns-wrapper" className="flex-row">
+      <div id="left-col" className="flex-half-width">
+        <h3>Left Column</h3>
+      </div>
+      <div id="right-col" className="flex-half-width">
+        <h3>Left Column</h3>
+      </div>
+    </section>
+
+  </main>
+);
 
 export default CodeEditor;
