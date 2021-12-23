bcab3cc93907906649e718a95a15c3de6331b997
Eric Laursen <ericjlaursen@gmail.com>
2021-12-23 09:17:48 -0500

    update component

--- src/Routes/MultiPartForm/Second/Second.js
+++ src/Routes/MultiPartForm/Second/Second.js
@@ -1,5 +1,25 @@
 import React from 'react';
+import Stats from './../Stats';
 
-const Second = () => (<p>Second</p>);
+const Second = ({previousStep, form: { firstname }, ...props}) => {
+    const thisUpdate = (e) => update(e.target.name, e.target.value);
+
+    const validate = () => {
+        if (confirm('Are you sure you want to go back?')) { // eslint-disable-line
+            previousStep();
+        }
+    };
+
+    return (
+        <div>
+            { firstname && <h3>Hey {firstname}! 👋</h3> }
+            I've added validation to the previous button.<br/>
+            <label>Last Name</label>
+            <input type='text' className='form-control' name='lastname' placeholder='Last Name'
+                onChange={thisUpdate} />
+            <Stats step={2} {...props} previousStep={validate} />
+        </div>
+    );
+};
 
 export default Second;
