c72fe7ec776748df8dafc8c9cef3b2c2cb0e5db4
Eric Laursen <ericjlaursen@gmail.com>
2021-12-23 09:17:41 -0500

    update component

--- src/Routes/MultiPartForm/Progress/Progress.js
+++ src/Routes/MultiPartForm/Progress/Progress.js
@@ -1,5 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Progress = () => (<p>Progress</p>);
+const Progress = (props) => {
+    const [state, updateState] = useState({
+        isActiveClass: '',
+        timeout: null,
+    });
+
+    useEffect(() => {
+        const { timeout } = state;
+
+        if (props.isActive && !timeout) {
+            updateState({
+                isActiveClass: 'loaded',
+                timeout: setTimeout(() => {
+                    props.nextStep();
+                }, 3000),
+            });
+        } else if (!props.isActive && timeout) {
+            clearTimeout(timeout);
+            updateState({
+                isActiveClass: '',
+                timeout: null,
+            });
+        }
+    });
+
+    return (
+        <div className={'progress-wrapper'}>
+            <p className='text-center'>Automated Progress...</p>
+            <div className={`progress ${state.isActiveClass}`}>
+                <div className={`${'progress-bar'} progress-bar-striped`} />
+            </div>
+        </div>
+    );
+};
 
 export default Progress;
