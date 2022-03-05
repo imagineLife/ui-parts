168b2178d658bbe11709c091f14b3e5ee2de4514
Eric Laursen <ericjlaursen@gmail.com>
2022-03-05 17:07:45 -0500

    more tiles more image imports

--- src/Components/TilesBox/TilesBox.js
+++ src/Components/TilesBox/TilesBox.js
@@ -3,7 +3,9 @@ import classNames from 'classnames';
 import SectionHeader from './../SectionHeader';
 import Image from './../Image';
 import Tile from './../Tile/';
-import Img  from './../assets/images/feature-tile-icon-01.svg'
+import StarImg  from './../assets/images/feature-tile-icon-01.svg'
+import CardImg from './../assets/images/feature-tile-icon-02.svg'
+import GlobeImg from './../assets/images/feature-tile-icon-03.svg'
 
 const TilesBox = ({
   className,
@@ -49,38 +51,34 @@ const TilesBox = ({
     >
       <div className="container">
         <div className={innerClasses}>
-          <SectionHeader data={sectionHeader} className="center-content" />
+          {/* <SectionHeader data={sectionHeader} className="center-content" /> */}
           <div className={tilesClasses}>
 
             <Tile 
               title="Common Words" 
               body="The go-to language that the author/orator leveraged"
               image={{
-                src: Img
+                src: StarImg
               }}
             />
 
-            <div className="tiles-item reveal-from-bottom" data-reveal-delay="200">
-              <div className="tiles-item-inner">
-                <div className="features-tiles-item-header">
-                  <div className="features-tiles-item-image mb-16">
-                    <Image
-                      src={require('./../assets/images/feature-tile-icon-02.svg')}
-                      alt="Features tile icon 02"
-                      width={64}
-                      height={64} />
-                  </div>
-                </div>
-                <div className="features-tiles-item-content">
-                  <h4 className="mt-0 mb-8">
-                    Robust Workflow
-                    </h4>
-                  <p className="m-0 text-sm">
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat.
-                    </p>
-                </div>
-              </div>
-            </div>
+            <Tile 
+              title="Longest Words" 
+              body="The breadth and complexity of the author/orators vocab"
+              image={{
+                src: CardImg
+              }}
+              delay={200}
+            />
+
+            <Tile 
+              title="Frequency of Words by Character-Length" 
+              body="How often the orator uses 2-letter words, 3-letter words, etc."
+              image={{
+                src: GlobeImg
+              }}
+              delay={400}
+            />
 
             <div className="tiles-item reveal-from-bottom" data-reveal-delay="400">
               <div className="tiles-item-inner">
