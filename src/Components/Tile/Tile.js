e15bc54ecd43d66f41efebf53d7e5e92f3d208b4
Eric Laursen <ericjlaursen@gmail.com>
2022-03-05 16:49:00 -0500

    conditional image

--- src/Components/Tile/Tile.js
+++ src/Components/Tile/Tile.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import Image from './../Image';
-const Tile = ({title, body}) => {
+const Tile = ({title, body, image}) => {
+  // prop validation
   if(!title || !body) throw `Update the Tile component with reqd props`
+  
+  // conditional img + props
+  let imgProps;
+  if(image){
+    imgProps = {
+      src: image.src,
+      alt: image.alt || 'Tile Image',
+      width: image.w || 64,
+      height: image.h || 64
+    }
+  }
   return (
     <div className="tiles-item reveal-from-bottom">
       <div className="tiles-item-inner">
         <div className="features-tiles-item-header">
-          <div className="features-tiles-item-image mb-16">
-            <Image
-              src={require('./../assets/images/feature-tile-icon-01.svg')}
-              alt="Features tile icon 01"
-              width={64}
-              height={64} />
-          </div>
+          {
+            image &&
+            <div className="features-tiles-item-image mb-16">
+              <React.Suspense fallback={<span></span>}>
+                <Image {...imgProps} />
+              </React.Suspense>
+            </div>
+          }
         </div>
         <div className="features-tiles-item-content">
           <h4 className="mt-0 mb-8">
