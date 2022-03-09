ad1fba005219667c997590dd8b7b5e99d1a51021
Eric Laursen <ericjlaursen@gmail.com>
2022-03-09 09:25:43 -0500

    conditional css export

--- webpack.config.js
+++ webpack.config.js
@@ -2,84 +2,81 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCss = require("mini-css-extract-plugin");
 const path = require('path');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
+module.exports = (a, props) => {
+  const devMode = props?.mode !== 'production';
+  return {
+    entry: './src/index.js',
+    output: {
+      filename: 'main.js',
 
-    //where to put the output, in the build dir
-    path: path.resolve(__dirname, 'build'),
-    //  https://webpack.js.org/configuration/output/#outputpublicpath
-    publicPath: '/'
-  },
-  module: {
-    rules: [
-      /*
-        JS with babel
-      */
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-
-      /*
-        HTML
-      */ 
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader"
+      //where to put the output, in the build dir
+      path: path.resolve(__dirname, 'build'),
+      //  https://webpack.js.org/configuration/output/#outputpublicpath
+      publicPath: '/'
+    },
+    module: {
+      rules: [
+        /*
+          JS with babel
+        */
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
           }
-        ]
-      },
+        },
 
-      // CSS
-      {
-        test: /\.css$/,
-        use: [MiniCss.loader, "css-loader"]
-      },
+        /*
+          HTML
+        */ 
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: "html-loader"
+            }
+          ]
+        },
 
-      // SCSS
-      {
-        test: /\.scss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
-        ]
-      },
+        // SCSS &/or css
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+            // Creates `style` nodes from JS strings
+            devMode ? "style-loader" : MiniCss.loader,
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+          ]
+        },
 
-      // json 
-      {
-        test: /\.(geo|topo)json$/,
-        loader: 'json-loader'
-      },
-      // files (non-svg-images)
-      {
-        test: /\.(png|jpg|gif|svg)$/i,
-        type: 'asset/resource'
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
-    }),
-    new MiniCss({
-      filename: '[name].css',
-      chunkFilename: '[id].css'
-    })
-  ],
-  devServer: {
-    historyApiFallback: true,
-    publicPath: '/'
+        // json 
+        {
+          test: /\.(geo|topo)json$/,
+          loader: 'json-loader'
+        },
+        // files (non-svg-images)
+        {
+          test: /\.(png|jpg|gif|svg)$/i,
+          type: 'asset/resource'
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+        filename: "./index.html"
+      }),
+      new MiniCss({
+        filename: '[name].css',
+        chunkFilename: '[id].css'
+      })
+    ],
+    devServer: {
+      historyApiFallback: true,
+      publicPath: '/'
+    }
   }
 };
