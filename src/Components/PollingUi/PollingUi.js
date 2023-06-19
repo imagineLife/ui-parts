b088fa06f79f83a73a25be5a25b5bd15528715f2
Eric Laursen <ericjlaursen@gmail.com>
2023-06-19 12:53:23 -0400

    init USING req.an.frm in ui

--- src/Components/PollingUi/PollingUi.js
+++ src/Components/PollingUi/PollingUi.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useCallback } from 'react';
+import useAnimationFrame from '../../Hooks/useAnimationFrame';
 
 // const API_HOST = process.env.API_HOST || 'localhost:3000';
 const API_HOST = 'http://localhost:3000';
@@ -14,51 +15,34 @@ const Message = (user, msg) => (
   </li>
 );
 
-// async function postMsgs(user, text) {
-//   const msgData = {
-//     user,
-//     text,
-//   };
+// async function getMsgs() {
+//   let jsonFromApi;
+//   try {
+//     const res = await fetch(POLLING_API_URL);
 
-//   const fetchOpts = {
-//     method: 'POST',
-//     body: JSON.stringify(msgData),
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//   };
+//     // something went wrong!
+//     if (res.status >= 400) {
+//       // error gets caught in the catch block below
+//       throw new Error('polling request errored with status', res.status);
+//     }
 
-//   return fetch(POLLING_API_URL, fetchOpts);
+//     const { msg } = await res.json();
+//     jsonFromApi = msg;
+//     // render(jsonFromApi);
+
+//     failedPollingCount = 0;
+//   } catch (e) {
+//     console.error('Polling error: ', e);
+//     failedPollingCount++;
+//   }
+
+//   /*
+//     setTImeout to poll is a more "trivial" approach
+//     requestAnimationFrame is a less persistent and more ui-aware approach
+//     THIS is replaced with rafFn below
+//   */
+//   // setTimeout(getMsgs, POLL_INTERVAL)
 // }
-
-async function getMsgs() {
-  let jsonFromApi;
-  try {
-    const res = await fetch(POLLING_API_URL);
-
-    // something went wrong!
-    if (res.status >= 400) {
-      // error gets caught in the catch block below
-      throw new Error('polling request errored with status', res.status);
-    }
-
-    const { msg } = await res.json();
-    jsonFromApi = msg;
-    // render(jsonFromApi);
-
-    failedPollingCount = 0;
-  } catch (e) {
-    console.error('Polling error: ', e);
-    failedPollingCount++;
-  }
-
-  /*
-    setTImeout to poll is a more "trivial" approach
-    requestAnimationFrame is a less persistent and more ui-aware approach
-    THIS is replaced with rafFn below
-  */
-  // setTimeout(getMsgs, POLL_INTERVAL)
-}
 
 
 
@@ -70,49 +54,23 @@ async function getMsgs() {
 
 
 const PollingUi = () => {
+
+  const [count, setCount] = useState(0);
+
+  const setCountFn = useCallback(
+    (deltaTime) =>
+      // Pass on a function to the setter of the state
+      // to make sure we always have the latest state
+      setCount((prevCount) => (prevCount + deltaTime * 0.001) % 100),
+    []
+  );
+
+  useAnimationFrame(setCountFn);
   return (
     <div>
       <h2>Polling UI</h2>
+      <p>Count: {count}</p>
     </div>
   );
 };
 export default PollingUi;
-
-/*
-  const chat = document.getElementById("chat");
-const msgs = document.getElementById("msgs");
-
-
-// a submit listener on the form in the HTML
-chat.addEventListener("submit", function (e) {
-  e.preventDefault();
-  postMsgs(chat.elements.user.value, chat.elements.text.value);
-  chat.elements.text.value = "";
-});
-
-function render(msgArr) {
-  // as long as msgsArr is holding all current messages, this will render them
-  // into the ui. yes, it's inefficent. yes, it's fine for this example
-  const html = msgArr.map(({ user, text, time, id }) => msgString(user, text, time, id));
-  msgs.innerHTML = html?.join("\n");
-}
-
-// A Trivial way to start the polling:
-// make the first request
-// REPLACED with code below
-// getMsgs();
-
-
-// a more browser-friendly version beside setTimeout
-// gets called ... a lot.... like a lot.....the POLL_INTERVAL keeps the fetch happening less frequently
-const BACKOFF_TIME = 5000;
-async function rafFn(s) {
-  if (timeToMakeNextPoll <= s) {
-    await getMsgs()
-    timeToMakeNextPoll = s + POLL_INTERVAL + (failedPollingCount * BACKOFF_TIME);
-  }
-  requestAnimationFrame(rafFn)
-}
-
-requestAnimationFrame(rafFn);
-*/
